0c19e9c16fc9fdbeb0b05b4572a4f2c2dbb3aec1
11VZ <carrytacker@gmail.com>
2025-04-28 16:18:58 -0400

    examples

---
+++ examples/function.fish
@@ -0,0 +1,3 @@
+public add(int a, int b) {
+    return a + b;
+}
---
+++ examples/survey.fish
@@ -0,0 +1,7 @@
+string day = input("how was your day?\n");
+if day == "good" {
+    print("I'm glad your day was good!");
+}
+if !day == "good" {
+    print("Your feeling " + day);
+}
